61d684fc9646c1c0d216f9c975bd0cd43c0cb5fd
SindhuKarnic <sindhukarnic@gmail.com>
2019-07-22 13:17:59 +0530

    timer with comments

--- Assessment/Frontend/timer.js
+++ Assessment/Frontend/timer.js
@@ -7,16 +7,21 @@ function startTimer() {
     //converting user input time to seconds
     var totalTime = uTime * 60;
         setInterval(function setTime() {
+            //Increment seconds until it is lesser than user input seconds
             if(totalSeconds <= totalTime-1) {
                 ++totalSeconds;
+                //To get seconds
                 sec.innerHTML = pad(totalSeconds % 60);
+                //to get minutes
                 min.innerHTML = pad(parseInt(totalSeconds / 60));
             }
         }, 1000);
 }
 
 function pad(val) {
+//converting to string
   var valString = val + "";
+  //if length is less than 2, append '0'
   if (valString.length < 2) {
     return "0" + valString;
   } else {
