75c29cec390e5ad9fce2e1b8c898a8b7cf044e1d
SindhuKarnic <sindhukarnic@gmail.com>
2019-08-23 16:19:29 +0530

    LMS BE

---
+++ Assignment/LMS/lms/src/main/java/com/tc/lms/dto/LibraryResponse.java
@@ -0,0 +1,38 @@
+package com.tc.lms.dto;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonRootName;
+
+@JsonRootName("library_response")
+public class LibraryResponse {
+	private int statusCode;
+	private String message;
+	private List<UserRegisterBean> beans;
+	public int getStatusCode() {
+		return statusCode;
+	}
+	public void setStatusCode(int statusCode) {
+		this.statusCode = statusCode;
+	}
+	public List<UserRegisterBean> getBeans() {
+		return beans;
+	}
+	public void setBeans(List<UserRegisterBean> beans) {
+		this.beans = beans;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getDecription() {
+		return decription;
+	}
+	public void setDecription(String decription) {
+		this.decription = decription;
+	}
+	private String decription;
+	
+}
